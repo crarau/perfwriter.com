3ae0853373b7f28a6453fa61292f9c8eac9f37a2
Ciprian (Chip) Rarau <ciprarau@gmail.com>
2024-09-15 21:19:28 -0700

    Replaced the pricing with Join the Waiting List

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,7 +4,8 @@ export const headerData = {
   links: [
     
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/PerfWriter', target: '_blank' }],
+  actions: [
+  ],
 };
 
 export const footerData = {
@@ -13,15 +14,8 @@ export const footerData = {
 
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/PerfWriter' },
   ],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> PerfWriter</a> · All rights reserved.
